9ad1525e7dbe26add3e5ec668f78946ba0ed3fd2
Odd-Arne Johansen <odd-arne.johansen@cerpus.com>
2020-10-14 08:23:44 +0200

    Change default index url

--- config/re-content-index.php
+++ config/re-content-index.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    "content-index-url" => env("RE_CONTENT_INDEX_URL", "http://recommendation-engine.local"),
+    "content-index-url" => env("RE_CONTENT_INDEX_URL", "http://re-content-index"),
 ];
